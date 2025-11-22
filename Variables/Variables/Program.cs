bd9927adc26fd3d4dccc79bb50640c5a8a7b5b38
Sharafiddin Mukhtorov <244348554+NurbekAkhmatov@users.noreply.github.com>
2025-11-22 12:44:53 +0500

    Add New lesson is Veriables

---
+++ Variables/Variables/Program.cs
@@ -0,0 +1,75 @@
+﻿//1
+string name = "Nurbek";
+int age = 22;
+string city = "Shahrisabz";
+Console.WriteLine($"My name is {name}, I am {age} years old and I live in {city}.");
+
+//2
+int a = 20;
+int b = 10;
+int sum = a + b;
+int difference = a - b;
+int product = a * b;
+double quotient = (double)a / b;
+Console.WriteLine($"Sum: {sum}, Difference: {difference}, Product: {product}, Quotient: {quotient}");
+
+//3
+string firstName = "Mening ismim Nurbek";
+string lastName = "Familiyam Axmadov";
+string fullName = "Men dasturchiman";
+Console.WriteLine($"{firstName},{lastName},{fullName}");
+
+
+//4
+bool isSunny = true;
+bool isRaining = false;
+Console.WriteLine($"Is it sunny today? {isSunny}");
+Console.WriteLine($"Is it raining today? {isRaining}");
+
+//5
+
+int a = 7;
+Console.WriteLine(4*a);
+Console.WriteLine(a*a);
+
+//6
+double loong = 14.5;
+double width = 8.2;
+double area = loong * width;
+Console.WriteLine($"Area of the rectangle: {area}");
+
+//7
+string s ="Hello ";
+string s1 ="World";
+Console.WriteLine(s+s1);
+
+//8
+string name = "Nurbek";
+int age = 22;
+double height = 1.75;
+bool isStudent = true;
+Console.WriteLine($"Name: {name}, Age: {age}, Height: {height}, Is Student: {isStudent}");
+
+//10
+double c;
+c = double.Parse(Console.ReadLine());
+double F = (double)c * 1.8 + 32;
+Console.WriteLine((double)F);
+
+//11
+double a = 12;
+double b = 5;
+double s = (a + b) / 2;
+Console.WriteLine(s);
+
+//12
+string str;
+str = Console.ReadLine();
+int length = str.Length;
+Console.WriteLine(length);
+
+//13
+string name = "Nurbek";
+int age = 22;
+double ball = 9.6;
+Console.WriteLine($"ISM: {name} , YOSH: {age} , BALL: {ball}");
